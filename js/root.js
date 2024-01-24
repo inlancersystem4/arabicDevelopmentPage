1f157b8f85e6f4d9eb16d64f713163e1cbc013e1
inlancersystem4 <inlancersystem4@gmail.com>
2024-01-24 18:40:01 +0530

    add files and folder and root fille and header code

---
+++ js/root.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                'themeColor1': '#685044',
+                'themeColor2': '#99B2DD',
+                'themeColor3': '#F9DEC9',
+                'themeColor4': '#3A405A',
+                'themeColor5': '#FFF',
+            },
+        }
+    }
+}
